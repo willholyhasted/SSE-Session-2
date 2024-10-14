ca468f9dc7c4a2ac2b8815d928d8a91759d19630
William Holy-Hasted <wh1124@oak02.doc.ic.ac.uk>
2024-10-14 16:13:31 +0100

    first try

---
+++ src/app.py
@@ -0,0 +1,18 @@
+#today is the 14 October 2024
+
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template("index.html")
+
+@app.route('/<name>')
+def print_name(name):
+    return f"Hello, {name}"
+
+@app.route("/submit", methods=["POST"])
+def submit():
+    input_name = request.form.get("name")
+    input_age = request.form.get("age")
+    return render_template("hello.html", name=input_name, age=input_age)
