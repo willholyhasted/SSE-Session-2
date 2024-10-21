7e6ed844e2ec8b2e88090afe9d7d3910b3b0986d
William Holy-Hasted <wh1124@im.ac.uk>
2024-10-21 16:36:42 +0100

    added app_test and new build

--- app.py
+++ app.py
@@ -18,3 +18,16 @@ def submit():
     input_name = request.form.get("name")
     input_age = request.form.get("age")
     return render_template("hello.html", name=input_name, age=input_age)
+
+@app.route("/query", methods =["GET"])
+def process_query_query():
+    query = request.args.get("q")
+    return process_query(query) 
+
+def process_query(string):
+    if string == "dinosaurs":
+        return "Dinosaurs ruled the Earth 200 million years ago"
+    elif string == "asteroids":
+        return "Unknown"
+    else:
+        return "Query not found"
