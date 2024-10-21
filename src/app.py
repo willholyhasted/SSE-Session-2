79ea2e8ed3cd68ea47c73ae71aa50e319a89dbee
William Holy-Hasted <wh1124@im.ac.uk>
2024-10-21 14:34:37 +0100

    second  attempt session 3

--- src/app.py
+++ src/app.py
@@ -1,15 +1,18 @@
-#today is the 14 October 2024
+
 
 from flask import Flask, render_template, request
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello_world():
     return render_template("index.html")
 
+
 @app.route('/<name>')
 def print_name(name):
     return f"Hello, {name}"
+
 
 @app.route("/submit", methods=["POST"])
 def submit():
